e2b2efb9b2980b338a7d133bc1a0db7f2f16fa94
MateoParra <mateoparra1171@gmail.com>
2024-07-27 01:56:40 -0500

    scroll-ui

--- app/components/RequestLocationDialog.tsx
+++ app/components/RequestLocationDialog.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
-import { cn } from '../lib/utils';
-import { Button } from './ui/Button';
+import { cn } from '../lib/utils'
+import { Button } from './ui/Button'
 
-import Image from 'next/image';
-import { Branch } from '../types/business';
+import Image from 'next/image'
+import { Branch } from '../types/business'
 import {
   IconCircle,
   IconCircleCheck,
   IconLocation,
   IconPinned,
-} from '@tabler/icons-react';
+} from '@tabler/icons-react'
 const RequestLocationDialog = ({
   view = 'grantPermissions',
   branches = [],
@@ -19,32 +19,32 @@ const RequestLocationDialog = ({
   denyLocation,
   onConfirm,
 }: {
-  branches?: (Branch | undefined)[];
-  open?: boolean;
-  view?: string;
-  getLocation: () => void;
-  denyLocation: () => void;
-  onConfirm: (branch: Branch | undefined) => void;
+  branches?: (Branch | undefined)[]
+  open?: boolean
+  view?: string
+  getLocation: () => void
+  denyLocation: () => void
+  onConfirm: (branch: Branch | undefined) => void
 }) => {
-  const [currentView, setCurrentView] = React.useState('grantPermissions');
+  const [currentView, setCurrentView] = React.useState('grantPermissions')
 
   useEffect(() => {
-    setCurrentView(view);
-  }, [view]);
+    setCurrentView(view)
+  }, [view])
 
   const goToSuggestedView = () => {
-    setCurrentView('suggestedLocations');
-  };
+    setCurrentView('suggestedLocations')
+  }
 
   const handleOnDeny = () => {
-    denyLocation();
-    goToSuggestedView();
-  };
+    denyLocation()
+    goToSuggestedView()
+  }
 
   const handleOnGrant = async () => {
-    await getLocation();
-    goToSuggestedView();
-  };
+    await getLocation()
+    goToSuggestedView()
+  }
 
   return (
     <div
@@ -55,33 +55,33 @@ const RequestLocationDialog = ({
       )}>
       {currentView == 'grantPermissions' && (
         <>
-          <div className="grow"></div>
+          <div className='grow'></div>
 
           <Image
             src={'/location.svg'}
             alt={'Permisos de ubicación'}
-            className="animate-bounce delay-[2000ms]"
+            className='animate-bounce delay-[2000ms]'
             width={120}
             height={120}
           />
 
-          <div className="flex flex-col items-center gap-3">
-            <h2 className="font-bold text-[1.5rem] text-sky-500 text-center">
+          <div className='flex flex-col items-center gap-3'>
+            <h2 className='font-bold text-[1.5rem] text-sky-500 text-center'>
               Mejora tu experiencia
             </h2>
-            <p className="text-center text-sky-900">
+            <p className='text-center text-sky-900'>
               Inicia compartiendo tu ubicación
             </p>
           </div>
-          <div className="grow"></div>
-          <div className="flex flex-col gap-3 w-full">
-            <Button onClick={handleOnGrant} className="w-full" type="button">
+          <div className='grow'></div>
+          <div className='flex flex-col gap-3 w-full'>
+            <Button onClick={handleOnGrant} className='w-full' type='button'>
               Compartir ubicación
             </Button>
             <Button
               onClick={handleOnDeny}
-              className="w-full"
-              type="button"
+              className='w-full'
+              type='button'
               variant={'secondary'}>
               Tal vez después
             </Button>
@@ -92,83 +92,83 @@ const RequestLocationDialog = ({
         <SuggestedLocations branches={branches} onConfirm={onConfirm} />
       )}
     </div>
-  );
-};
+  )
+}
 
 const SuggestedLocations = ({
   branches,
   onConfirm,
 }: {
-  toggleView?: () => void;
-  onConfirm: (branch: Branch | undefined) => void;
-  branches: (Branch | undefined)[];
+  toggleView?: () => void
+  onConfirm: (branch: Branch | undefined) => void
+  branches: (Branch | undefined)[]
 }) => {
   const getNormalizedBusinessName = (name: string | undefined) => {
     if (!name) {
-      return '';
+      return ''
     }
-    return name.toLocaleLowerCase().split(' ').join('-');
-  };
+    return name.toLocaleLowerCase().split(' ').join('-')
+  }
 
-  const [selected, setSelected] = useState<string>();
+  const [selected, setSelected] = useState<string>()
 
   useEffect(() => {
-    if (branches.length == 0) return;
-    setSelected(getNormalizedBusinessName(branches[0]?.Name));
-  }, [branches]);
+    if (branches.length == 0) return
+    setSelected(getNormalizedBusinessName(branches[0]?.Name))
+  }, [branches])
 
   const handleClickSelected = (branchName: string | undefined) => {
-    setSelected(getNormalizedBusinessName(branchName));
-  };
+    setSelected(getNormalizedBusinessName(branchName))
+  }
   return (
-    <div className="flex flex-col w-full h-full justify-between items-center ">
-      <div className="grow"></div>
-      <div className="flex flex-col items-center gap-3">
+    <div className='flex flex-col w-full h-full justify-between items-center '>
+      <div className='grow'></div>
+      <div className='flex flex-col items-center gap-3'>
         <Image
           src={'/location.svg'}
           alt={'Permisos de ubicación'}
-          className="animate-bounce delay-[2000ms]"
+          className='animate-bounce delay-[2000ms]'
           width={120}
           height={120}
         />
-        <h2 className="font-bold text-[1.5rem] text-sky-500 text-center">
+        <h2 className='font-bold text-[1.5rem] text-sky-500 text-center'>
           ¿Dónde te encuentras?
         </h2>
-        <p className="text-center text-sky-900">
+        <p className='text-center text-sky-900 mb-3'>
           Selecciona en qué sucursal estás
         </p>
       </div>
-      <div className="grow"></div>
+      <div className='grow'></div>
 
-      <div className="flex flex-col gap-3 w-full">
+      <div className='flex flex-col gap-3 w-full overflow-y-scroll'>
         {branches.map((branch) => {
           return (
             <div
               onClick={() => handleClickSelected(branch?.Name)}
-              className="flex items-center gap-4 border py-2 px-3 rounded-lg cursor-pointer focus:ring"
+              className='flex items-center gap-4 border py-2 px-3 rounded-lg cursor-pointer focus:ring'
               key={branch?.Name}>
               {selected == getNormalizedBusinessName(branch?.Name ?? '') ? (
-                <IconCircleCheck size={14} className="text-qik" />
+                <IconCircleCheck size={14} className='text-qik' />
               ) : (
                 <IconCircle size={14} />
               )}
-              <div className="flex flex-col">
-                <h4 className="text-qik text-[1rem] font-bold">
+              <div className='flex flex-col'>
+                <h4 className='text-qik text-[1rem] font-bold'>
                   {branch?.Name}
                 </h4>
-                <div className="flex items-center gap-1">
+                <div className='flex items-center gap-1'>
                   <IconPinned size={10} />
-                  <p className="text-sky-900 text-[0.7rem] font-light">
+                  <p className='text-sky-900 text-[0.7rem] font-light'>
                     {branch?.Address}
                   </p>
                 </div>
               </div>
             </div>
-          );
+          )
         })}
       </div>
 
-      <div className="flex flex-col gap-3 w-full mt-10">
+      <div className='flex flex-col gap-3 w-full mt-10'>
         <Button
           onClick={() =>
             onConfirm(
@@ -177,13 +177,13 @@ const SuggestedLocations = ({
               )
             )
           }
-          className="w-full"
-          type="button">
+          className='w-full'
+          type='button'>
           ¡Aquí estoy!
         </Button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default RequestLocationDialog;
+export default RequestLocationDialog
