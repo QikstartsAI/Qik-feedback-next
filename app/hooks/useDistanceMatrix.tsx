4e17485fcbecfdf0739512b2bbc83030ef498d97
Daniel Erazo <69876673+Danii2020@users.noreply.github.com>
2024-07-30 11:05:13 -0500

    disabling maps api (#18)
    Co-authored-by: Emanuel Olaya <emanuelolaya@hotmail.com>

--- app/hooks/useDistanceMatrix.tsx
+++ app/hooks/useDistanceMatrix.tsx
@@ -4,9 +4,9 @@ import { Branch } from '../types/business'
 export const useDistanceMatrix = () => {
   const [origin, setOrigin] = useState<{ latitude: number | null; longitude: number | null }>({ latitude: null, longitude: null })
   const [destinations, setDestinations] = useState<Branch[]>([])
-  const [closestDestination, setClosestDestination] = useState<Branch>()
-  const coordenates = destinations.map((destination) => destination.Address)
-  const [data, setData] = useState<google.maps.DistanceMatrixResponse | null>(null)
+  const [closestDestination, setClosestDestination] = useState<Branch | Branch[]>()
+  // const coordenates = destinations.map((destination) => destination.Address)
+  // const [data, setData] = useState<google.maps.DistanceMatrixResponse | null>(null)
 
   const setDistanceMatrix = useCallback(({ origin, destinations = [] }: { origin: { latitude: number | null; longitude: number | null }; destinations?: Branch[] }) => {
     setOrigin(origin)
@@ -19,63 +19,66 @@ export const useDistanceMatrix = () => {
         return
       }
 
-      const originInCoordinates = new google.maps.LatLng(origin.latitude, origin.longitude)
-      const service = new google.maps.DistanceMatrixService()
-      service.getDistanceMatrix(
-        {
-          origins: [originInCoordinates],
-          destinations: coordenates,
-          travelMode: google.maps.TravelMode.DRIVING,
-          unitSystem: google.maps.UnitSystem.METRIC,
-        },
-        (response, status) => {
-          setData(response)
-          if (response === null) {
-            return
-          }
+      setClosestDestination(destinations);
 
-          const elements = response.rows[0].elements;
+      // const originInCoordinates = new google.maps.LatLng(origin.latitude, origin.longitude)
+      // const service = new google.maps.DistanceMatrixService()
+      // service.getDistanceMatrix(
+      //   {
+      //     origins: [originInCoordinates],
+      //     destinations: coordenates,
+      //     travelMode: google.maps.TravelMode.DRIVING,
+      //     unitSystem: google.maps.UnitSystem.METRIC,
+      //   },
+      //   (response, status) => {
+      //     setData(response)
+      //     if (response === null) {
+      //       return
+      //     }
 
-          // Filtramos los elementos que tienen el estado "OK" y obtenemos sus distancias
-          const validElements = elements
-            .map((element, index) => ({ ...element, index }))
-            .filter((element: google.maps.DistanceMatrixResponseElement & { index: number }) => element.status === 'OK');
+      //     const elements = response.rows[0].elements;
 
-          // Verificamos si todos los elementos son "ZERO_RESULTS" o "NOT_FOUND"
-          const allZeroResultsOrNotFound = validElements.length === 0;
+      //     // Filtramos los elementos que tienen el estado "OK" y obtenemos sus distancias
+      //     const validElements = elements
+      //       .map((element, index) => ({ ...element, index }))
+      //       .filter((element: google.maps.DistanceMatrixResponseElement & { index: number }) => element.status === 'OK');
 
-          if (allZeroResultsOrNotFound) {
-            console.error('Si hay origen pero por la lejania no se puede obtener las distancias de las demas sucursales');
-            return;
-          }
+      //     // Verificamos si todos los elementos son "ZERO_RESULTS" o "NOT_FOUND"
+      //     const allZeroResultsOrNotFound = validElements.length === 0;
 
-          // Obtenemos las distancias de los elementos válidos
-          const distanceArr: number[] = validElements.map(
-            (element: google.maps.DistanceMatrixResponseElement) => element.distance.value
-          );
+      //     if (allZeroResultsOrNotFound) {
+      //       console.error('Si hay origen pero por la lejania no se puede obtener las distancias de las demas sucursales');
+      //       return;
+      //     }
 
-          if (distanceArr.length === 0) {
-            return;
-          }
+      //     // Obtenemos las distancias de los elementos válidos
+      //     const distanceArr: number[] = validElements.map(
+      //       (element: google.maps.DistanceMatrixResponseElement) => element.distance.value
+      //     );
 
-          // Encontramos la distancia mínima y el índice del elemento correspondiente
-          const minDistance: number = Math.min(...distanceArr);
-          const closerBranchIndex = validElements.find(
-            (element: google.maps.DistanceMatrixResponseElement) => element.distance.value === minDistance
-          )?.index;
+      //     if (distanceArr.length === 0) {
+      //       return;
+      //     }
 
-          if (closerBranchIndex === undefined) {
-            console.error('No se encontró una sucursal más cercana.');
-            return;
-          }
+      //     // Encontramos la distancia mínima y el índice del elemento correspondiente
+      //     const minDistance: number = Math.min(...distanceArr);
+      //     const closerBranchIndex = validElements.find(
+      //       (element: google.maps.DistanceMatrixResponseElement) => element.distance.value === minDistance
+      //     )?.index;
 
-          setClosestDestination(destinations[closerBranchIndex]);
-        }
-      )
+      //     if (closerBranchIndex === undefined) {
+      //       console.error('No se encontró una sucursal más cercana.');
+      //       return;
+      //     }
+
+      //     setClosestDestination(destinations[closerBranchIndex]);
+      //   }
+      // )
     } catch (err) {
       console.error('error al hacer el fetching de datos: ', err)
     }
-  }, [origin, coordenates, destinations])
+  //}, [origin, coordenates, destinations])
+   }, [origin, destinations])
 
   useEffect(() => {
     if (origin.latitude == null || origin.longitude == null || destinations.length === 0) {
