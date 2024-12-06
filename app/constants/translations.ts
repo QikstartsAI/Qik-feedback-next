3b942391d0a649fa8987261dc5e2e09d2935d144
Ricardo Mancero <106605161+rmancero11@users.noreply.github.com>
2024-12-06 09:44:44 -0500

    Update label (#28)
    Co-authored-by: Jesus Sanchez <jhsanro@gmail.com>

--- app/constants/translations.ts
+++ app/constants/translations.ts
@@ -1,304 +1,309 @@
 const translations = {
   formTitle: {
-    US: 'We value your opinion 😊, it will take you less than ',
-    CA: 'Nous apprécions votre opinion 😊, cela vous prendra moins de ',
-    ES: 'Valoramos tu opinión 😊, te llevará menos de ',
-    default: 'Valoramos tu opinión 😊, te llevará menos de '
+    US: "We value your opinion 😊, it will take you less than ",
+    CA: "Nous apprécions votre opinion 😊, cela vous prendra moins de ",
+    ES: "Valoramos tu opinión 😊, te llevará menos de ",
+    default: "Valoramos tu opinión 😊, te llevará menos de ",
   },
   formSubTitle: {
-    'US': '1 minute',
-    'CA': '1 minute',
-    'ES': '1 minuto',
-    'default': '1 minuto'
+    US: "1 minute",
+    CA: "1 minute",
+    ES: "1 minuto",
+    default: "1 minuto",
   },
   fullNameQuestion: {
-    'US': 'Full Name',
-    'CA': 'Nom complet',
-    'ES': 'Nombre completo',
-    'default': 'Nombre completo'
+    US: "Full Name",
+    CA: "Nom complet",
+    ES: "Nombre completo",
+    default: "Nombre completo",
   },
   birthdayQuestion: {
-    'US': 'Your birthday? 🎂 (optional)',
-    'CA': 'Ton anniversaire? 🎂 (facultatif)',
-    'ES': '¿Tu fecha de cumpleaños? 🎂 (opcional)',
-    'default': '¿Tu fecha de cumpleaños? 🎂 (opcional)'
+    US: "Your birthday? 🎂 (optional)",
+    CA: "Ton anniversaire? 🎂 (facultatif)",
+    ES: "¿Tu fecha de cumpleaños? 🎂 (opcional)",
+    default: "¿Tu fecha de cumpleaños? 🎂 (opcional)",
   },
   phoneNumberQuestion: {
-    'US': 'Phone (optional)',
-    'CA': 'Téléphone (facultatif)',
-    'ES': 'Teléfono (opcional)',
-    'default': 'Teléfono (opcional)'
+    US: "Phone",
+    CA: "Téléphone",
+    ES: "Teléfono",
+    default: "Teléfono",
   },
   emailQuestion: {
-    'US': 'Email',
-    'CA': 'Courrier électronique',
-    'ES': 'Correo electrónico',
-    'default': 'Correo electrónico'
+    US: "Email",
+    CA: "Courrier électronique",
+    ES: "Correo electrónico",
+    default: "Correo electrónico",
   },
   courtesyQuestion: {
-    'US': 'How would you rate the kindness and courtesy of the Hooters staff during your visit?',
-    'CA': 'Comment évaluez-vous la gentillesse et la courtoisie du personnel de Hooters pendant votre visite ?',
-    'ES': '¿Cómo calificarías la atención y cortesía del personal de Hooters durante tu visita?',
-    'default': '¿Cómo calificarías la atención y cortesía del personal de Hooters durante tu visita?'
+    US: "How would you rate the kindness and courtesy of the Hooters staff during your visit?",
+    CA: "Comment évaluez-vous la gentillesse et la courtoisie du personnel de Hooters pendant votre visite ?",
+    ES: "¿Cómo calificarías la atención y cortesía del personal de Hooters durante tu visita?",
+    default:
+      "¿Cómo calificarías la atención y cortesía del personal de Hooters durante tu visita?",
   },
   placeCleannessQuestion: {
-    'US': 'How do you rate the place cleanness?',
-    'CA': 'Comment évaluez-vous la propreté du lieu ?',
-    'ES': '¿Cómo calificas la limpieza del lugar?',
-    'default': '¿Cómo calificas la limpieza del lugar?'
+    US: "How do you rate the place cleanness?",
+    CA: "Comment évaluez-vous la propreté du lieu ?",
+    ES: "¿Cómo calificas la limpieza del lugar?",
+    default: "¿Cómo calificas la limpieza del lugar?",
   },
   quicknessQuestion: {
-    'US': 'How would you rate the speed at which you were served and how quickly your food and drinks were brought to you?',
-    'CA': 'Comment évaluez-vous la rapidité du service et la rapidité avec laquelle vos plats et boissons ont été apportés ?',
-    'ES': '¿Cómo calificas la rapidez con la que te atendieron y te trajeron tus alimentos y bebidas?',
-    'default': '¿Cómo calificas la rapidez con la que te atendieron y te trajeron tus alimentos y bebidas?'
+    US: "How would you rate the speed at which you were served and how quickly your food and drinks were brought to you?",
+    CA: "Comment évaluez-vous la rapidité du service et la rapidité avec laquelle vos plats et boissons ont été apportés ?",
+    ES: "¿Cómo calificas la rapidez con la que te atendieron y te trajeron tus alimentos y bebidas?",
+    default:
+      "¿Cómo calificas la rapidez con la que te atendieron y te trajeron tus alimentos y bebidas?",
   },
   foodQualityQuestion: {
-    'US': 'How would you rate the overall taste and quality of your food and drinks?',
-    'CA': 'Comment évaluez-vous le goût et la qualité générale de vos plats et boissons ?',
-    'ES': '¿Cómo calificas el sabor y la calidad en general de tus alimentos y bebidas?',
-    'default': '¿Cómo calificas el sabor y la calidad en general de tus alimentos y bebidas?'
+    US: "How would you rate the overall taste and quality of your food and drinks?",
+    CA: "Comment évaluez-vous le goût et la qualité générale de vos plats et boissons ?",
+    ES: "¿Cómo calificas el sabor y la calidad en general de tus alimentos y bebidas?",
+    default:
+      "¿Cómo calificas el sabor y la calidad en general de tus alimentos y bebidas?",
   },
   ambienceQuestion: {
-    'US': 'How would you rate the restaurant ambience?',
-    'CA': 'Comment évaluez-vous l\'ambiance du restaurant ?',
-    'ES': '¿Cómo calificas el ambiente del restaurante?',
-    'default': '¿Cómo calificas el ambiente del restaurante?'
+    US: "How would you rate the restaurant ambience?",
+    CA: "Comment évaluez-vous l'ambiance du restaurant ?",
+    ES: "¿Cómo calificas el ambiente del restaurante?",
+    default: "¿Cómo calificas el ambiente del restaurante?",
   },
   experienceQuestion: {
-    'US': 'How would you rate your experience at Hooters?',
-    'CA': 'Comment évaluez-vous votre expérience chez Hooters ?',
-    'ES': '¿Cómo calificas tu experiencia en Hooters?',
-    'default': '¿Cómo calificas tu experiencia en Hooters?'
+    US: "How would you rate your experience at Hooters?",
+    CA: "Comment évaluez-vous votre expérience chez Hooters ?",
+    ES: "¿Cómo calificas tu experiencia en Hooters?",
+    default: "¿Cómo calificas tu experiencia en Hooters?",
   },
   recommendingQuestion: {
-    'US': 'Would you recommend Hooters to friends and family?',
-    'CA': 'Recommanderiez-vous Hooters à vos amis et à votre famille ?',
-    'ES': '¿Recomendarías a Hooters a amigos y familiares?',
-    'default': '¿Recomendarías a Hooters a amigos y familiares?'
+    US: "Would you recommend Hooters to friends and family?",
+    CA: "Recommanderiez-vous Hooters à vos amis et à votre famille ?",
+    ES: "¿Recomendarías a Hooters a amigos y familiares?",
+    default: "¿Recomendarías a Hooters a amigos y familiares?",
   },
   comeBackQuestion: {
-    'US': 'Would you come back to Hooters?',
-    'CA': 'Reviendriez-vous à Hooters ?',
-    'ES': '¿Regresarías a Hooters?',
-    'default': '¿Regresarías a Hooters?'
+    US: "Would you come back to Hooters?",
+    CA: "Reviendriez-vous à Hooters ?",
+    ES: "¿Regresarías a Hooters?",
+    default: "¿Regresarías a Hooters?",
   },
   oneStarLabel: {
-    'US': 'Bad',
-    'CA': 'Mauvais',
-    'ES': 'Malo',
-    'default': 'Malo'
+    US: "Bad",
+    CA: "Mauvais",
+    ES: "Malo",
+    default: "Malo",
   },
   twoStarLabel: {
-    'US': 'Regular',
-    'CA': 'Régulier',
-    'ES': 'Regular',
-    'default': 'Regular'
+    US: "Regular",
+    CA: "Régulier",
+    ES: "Regular",
+    default: "Regular",
   },
   threeStarLabel: {
-    'US': 'Good',
-    'CA': 'Bon',
-    'ES': 'Bien',
-    'default': 'Bien'
+    US: "Good",
+    CA: "Bon",
+    ES: "Bien",
+    default: "Bien",
   },
   fourStarLabel: {
-    'US': 'Very Good',
-    'CA': 'Très Bon',
-    'ES': 'Muy Bien',
-    'default': 'Muy Bien'
+    US: "Very Good",
+    CA: "Très Bon",
+    ES: "Muy Bien",
+    default: "Muy Bien",
   },
   fiveStarLabel: {
-    'US': 'Excellent',
-    'CA': 'Excellent',
-    'ES': 'Excelente',
-    'default': 'Excelente'
+    US: "Excellent",
+    CA: "Excellent",
+    ES: "Excelente",
+    default: "Excelente",
   },
   nextButton: {
-    'US': 'Next',
-    'CA': 'Suivant',
-    'ES': 'Siguiente',
-    'default': 'Siguiente'
+    US: "Next",
+    CA: "Suivant",
+    ES: "Siguiente",
+    default: "Siguiente",
   },
   yesButton: {
-    'US': 'Yes',
-    'CA': 'Oui',
-    'ES': 'Sí',
-    'default': 'Sí'
+    US: "Yes",
+    CA: "Oui",
+    ES: "Sí",
+    default: "Sí",
   },
   noButton: {
-    'US': 'No',
-    'CA': 'Non',
-    'ES': 'No',
-    'default': 'No'
+    US: "No",
+    CA: "Non",
+    ES: "No",
+    default: "No",
   },
   submitButton: {
-    'US': 'Submit',
-    'CA': 'Envoyer',
-    'ES': 'Enviar',
-    'default': 'Enviar'
+    US: "Submit",
+    CA: "Envoyer",
+    ES: "Enviar",
+    default: "Enviar",
   },
   whyText: {
-    'US': 'Why?',
-    'CA': 'Pourquoi ?',
-    'ES': '¿Por qué?',
-    'default': '¿Por qué?'
+    US: "Why?",
+    CA: "Pourquoi ?",
+    ES: "¿Por qué?",
+    default: "¿Por qué?",
   },
   recommendingPlaceholder: {
-    'US': 'Ex: the food was very good and the service excellent...',
-    'CA': 'Ex: la nourriture était très bonne et le service excellent...',
-    'ES': 'Ej: la comida estuvo muy deliciosa y el servicio estupendo...',
-    'default': 'Ej: la comida estuvo muy deliciosa y el servicio estupendo...'
+    US: "Ex: the food was very good and the service excellent...",
+    CA: "Ex: la nourriture était très bonne et le service excellent...",
+    ES: "Ej: la comida estuvo muy deliciosa y el servicio estupendo...",
+    default: "Ej: la comida estuvo muy deliciosa y el servicio estupendo...",
   },
   noRecommendingPlaceholder: {
-    'US': 'Ex: I wouldn\'t recommend it because...',
-    'CA': 'Ex: Je ne le recommanderais pas parce que...',
-    'ES': 'Ej: No lo recomendaría porque...',
-    'default': 'Ej: No lo recomendaría porque...'
+    US: "Ex: I wouldn't recommend it because...",
+    CA: "Ex: Je ne le recommanderais pas parce que...",
+    ES: "Ej: No lo recomendaría porque...",
+    default: "Ej: No lo recomendaría porque...",
   },
   submitText1: {
-    'US': 'By pressing ',
-    'CA': 'En pressant ',
-    'ES': 'Al presionar ',
-    'default': 'Al presionar '
+    US: "By pressing ",
+    CA: "En pressant ",
+    ES: "Al presionar ",
+    default: "Al presionar ",
   },
   submitText2: {
-    'US': ', you\'ll go to ',
-    'CA': ', vous irez sur ',
-    'ES': ', irás a ',
-    'default': ', irás a ',
+    US: ", you'll go to ",
+    CA: ", vous irez sur ",
+    ES: ", irás a ",
+    default: ", irás a ",
   },
   submitText3: {
-    'US': ' and if you leave us ',
-    'CA': ' et si vous nous laissez des ',
-    'ES': ' y si nos dejas ',
-    'default': ' y si nos dejas ',
+    US: " and if you leave us ",
+    CA: " et si vous nous laissez des ",
+    ES: " y si nos dejas ",
+    default: " y si nos dejas ",
   },
   submitText4: {
-    'US': 'stars ',
-    'CA': 'étoiles ',
-    'ES': 'estrellas ',
-    'default': 'estrellas ',
+    US: "stars ",
+    CA: "étoiles ",
+    ES: "estrellas ",
+    default: "estrellas ",
   },
   submitText5: {
-    'US': 'it would be great!',
-    'CA': 'ce serait génial !',
-    'ES': 'sería genial!',
-    'default': 'sería genial!',
+    US: "it would be great!",
+    CA: "ce serait génial !",
+    ES: "sería genial!",
+    default: "sería genial!",
   },
   toImproveText: {
-    'US': 'What do you think we could improve?',
-    'CA': 'Selon vous, que pourrions-nous améliorer ?',
-    'ES': '¿En qué crees que podemos mejorar?',
-    'default': '¿En qué crees que podemos mejorar?',
+    US: "What do you think we could improve?",
+    CA: "Selon vous, que pourrions-nous améliorer ?",
+    ES: "¿En qué crees que podemos mejorar?",
+    default: "¿En qué crees que podemos mejorar?",
   },
   foodButton: {
-    'US': 'Food',
-    'CA': 'Nourriture',
-    'ES': 'Comida',
-    'default': 'Comida',
+    US: "Food",
+    CA: "Nourriture",
+    ES: "Comida",
+    default: "Comida",
   },
   serviceButton: {
-    'US': 'Service',
-    'CA': 'Service',
-    'ES': 'Servicio',
-    'default': 'Servicio',
+    US: "Service",
+    CA: "Service",
+    ES: "Servicio",
+    default: "Servicio",
   },
   ambienceButton: {
-    'US': 'Ambience',
-    'CA': 'Ambiance',
-    'ES': 'Ambiente',
-    'default': 'Ambiente',
+    US: "Ambience",
+    CA: "Ambiance",
+    ES: "Ambiente",
+    default: "Ambiente",
   },
   shareDetailsText: {
-    'US': 'Share details about your experience in ',
-    'CA': 'Partagez des détails sur votre expérience dans ',
-    'ES': 'Compártenos detalles sobre tu experiencia en ',
-    'default': 'Compártenos detalles sobre tu experiencia en ',
+    US: "Share details about your experience in ",
+    CA: "Partagez des détails sur votre expérience dans ",
+    ES: "Compártenos detalles sobre tu experiencia en ",
+    default: "Compártenos detalles sobre tu experiencia en ",
   },
   shareDetailsPlaceholder: {
-    'US': 'Ex: The food was delicious but the service was slow...',
-    'CA': 'Ex: La nourriture était délicieuse mais le service était lent. . .',
-    'ES': 'Ej: La comida estuvo rica pero el servicio estuvo lento...',
-    'default': 'Ej: La comida estuvo rica pero el servicio estuvo lento...'
+    US: "Ex: The food was delicious but the service was slow...",
+    CA: "Ex: La nourriture était délicieuse mais le service était lent. . .",
+    ES: "Ej: La comida estuvo rica pero el servicio estuvo lento...",
+    default: "Ej: La comida estuvo rica pero el servicio estuvo lento...",
   },
   termsAndConditions1: {
-    'US': 'By pressing "Submit", I declare that I accept the ',
-    'CA': 'En pressant "Enviar", déclarez que vous acceptez les ',
-    'ES': 'Al presionar "Enviar", declaro que acepto los ',
-    'default': 'Al presionar "Enviar", declaro que acepto los '
+    US: 'By pressing "Submit", I declare that I accept the ',
+    CA: 'En pressant "Enviar", déclarez que vous acceptez les ',
+    ES: 'Al presionar "Enviar", declaro que acepto los ',
+    default: 'Al presionar "Enviar", declaro que acepto los ',
   },
   termsAndConditions2: {
-    'US': 'Terms and Conditions',
-    'CA': "Conditions générales d'utilisation",
-    'ES': 'Términos y Condiciones',
-    'default': 'Términos y Condiciones'
+    US: "Terms and Conditions",
+    CA: "Conditions générales d'utilisation",
+    ES: "Términos y Condiciones",
+    default: "Términos y Condiciones",
   },
   termsAndConditions3: {
-    'US': ' and the ',
-    'CA': ' et là ',
-    'ES': ' y las ',
-    'default': ' y las '
+    US: " and the ",
+    CA: " et là ",
+    ES: " y las ",
+    default: " y las ",
   },
   termsAndConditions4: {
-    'US': ' Privacy Policies',
-    'CA': ' Politiques de confidentialité',
-    'ES': ' Políticas de Privacidad',
-    'default': ' Políticas de Privacidad'
+    US: " Privacy Policies",
+    CA: " Politiques de confidentialité",
+    ES: " Políticas de Privacidad",
+    default: " Políticas de Privacidad",
   },
   formErrorMessage: {
-    'US': 'Give us a rating before continuing',
-    'CA': "Donnez-nous une note avant de continuer",
-    'ES': 'Danos una calificación antes de seguir',
-    'default': 'Danos una calificación antes de seguir'
+    US: "Give us a rating before continuing",
+    CA: "Donnez-nous une note avant de continuer",
+    ES: "Danos una calificación antes de seguir",
+    default: "Danos una calificación antes de seguir",
   },
   emptyRecommendingError: {
-    'US': 'Please tell us why you would recommend Hooters',
-    'CA': 'Veuillez nous dire pourquoi vous recommanderiez Hooters',
-    'ES': 'Por favor dinos por qué recomendarías Hooters',
-    'default': 'Por favor dinos por qué recomendarías Hooters'
+    US: "Please tell us why you would recommend Hooters",
+    CA: "Veuillez nous dire pourquoi vous recommanderiez Hooters",
+    ES: "Por favor dinos por qué recomendarías Hooters",
+    default: "Por favor dinos por qué recomendarías Hooters",
   },
   emptyNoRecommendingError: {
-    'US': 'Please tell us why you wouldn\'t recommend Hooters',
-    'CA': 'Veuillez nous dire pourquoi vous ne recommanderiez pas Hooters',
-    'ES': 'Por favor dinos por qué no recomendarías Hooters',
-    'default': 'Por favor dinos por qué no recomendarías Hooters'
+    US: "Please tell us why you wouldn't recommend Hooters",
+    CA: "Veuillez nous dire pourquoi vous ne recommanderiez pas Hooters",
+    ES: "Por favor dinos por qué no recomendarías Hooters",
+    default: "Por favor dinos por qué no recomendarías Hooters",
   },
   chooseOneOptionError: {
-    'US': 'Choose one option',
-    'CA': 'Choisissez une option',
-    'ES': 'Selecciona al menos una opción',
-    'default': 'Selecciona al menos una opción'
+    US: "Choose one option",
+    CA: "Choisissez une option",
+    ES: "Selecciona al menos una opción",
+    default: "Selecciona al menos una opción",
   },
   howToImprovementError: {
-    'US': 'Please tell us how we can improve',
-    'CA': 'Veuillez nous dire comment nous pouvons nous améliorer',
-    'ES': 'Por favor, escribe en que podemos mejorar',
-    'default': 'Por favor, escribe en que podemos mejorar'
+    US: "Please tell us how we can improve",
+    CA: "Veuillez nous dire comment nous pouvons nous améliorer",
+    ES: "Por favor, escribe en que podemos mejorar",
+    default: "Por favor, escribe en que podemos mejorar",
   },
   whyComeBackError: {
-    'US': 'Please tell us why you would come back',
-    'CA': 'Veuillez nous dire pourquoi vous reviendriez',
-    'ES': 'Por favor dinos por qué volverías',
-    'default': 'Por favor dinos por qué volverías'
+    US: "Please tell us why you would come back",
+    CA: "Veuillez nous dire pourquoi vous reviendriez",
+    ES: "Por favor dinos por qué volverías",
+    default: "Por favor dinos por qué volverías",
   },
   feedbackLimit: {
-    'US': 'Thank you! ✌🏻 You have reached the daily survey limit. Until your next visit! 😉',
-    'CA': "Merci! ✌🏻 Vous avez atteint la limite quotidienne d'enquêtes. A votre prochaine visite ! 😉",
-    'ES': 'Has alcanzado el límite diario de encuestas. ¡Hasta tu próxima visita! 😉',
-    'default': 'Has alcanzado el límite diario de encuestas. ¡Hasta tu próxima visita! 😉'
+    US: "Thank you! ✌🏻 You have reached the daily survey limit. Until your next visit! 😉",
+    CA: "Merci! ✌🏻 Vous avez atteint la limite quotidienne d'enquêtes. A votre prochaine visite ! 😉",
+    ES: "Has alcanzado el límite diario de encuestas. ¡Hasta tu próxima visita! 😉",
+    default:
+      "Has alcanzado el límite diario de encuestas. ¡Hasta tu próxima visita! 😉",
   },
   formUserDataErrorMessage: {
-    'US': 'Please, fill the user data fields before continuing',
-    'CA': "Veuillez remplir les champs de données utilisateur avant de continuer",
-    'ES': 'Por favor, llena tus datos antes de continuar',
-    'default': 'Por favor, llena tus datos antes de continuar'
+    US: "Please, fill the user data fields before continuing",
+    CA: "Veuillez remplir les champs de données utilisateur avant de continuer",
+    ES: "Por favor, llena tus datos antes de continuar",
+    default: "Por favor, llena tus datos antes de continuar",
   },
   formUserPhoneNumberDataErrorMessage: {
-    'US': 'Please, fill your phone number field before continuing',
-    'CA': "Veuillez remplir le champ de votre numéro de téléphone avant de continuer",
-    'ES': 'Por favor, llena el campo de tu número de teléfono antes de continuar',
-    'default': 'Por favor, llena el campo de tu número de teléfono antes de continuar'
-  }
+    US: "Please, fill your phone number field before continuing",
+    CA: "Veuillez remplir le champ de votre numéro de téléphone avant de continuer",
+    ES: "Por favor, llena el campo de tu número de teléfono antes de continuar",
+    default:
+      "Por favor, llena el campo de tu número de teléfono antes de continuar",
+  },
 };
 export default translations;
 
