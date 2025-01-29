93a6cb9a59640307120ad9c32a6b0d9db9def7b3
Jesus Sanchez <jhsanro@gmail.com>
2025-01-28 19:40:11 -0500

    Fix text in italian

--- app/constants/placeholders.ts
+++ app/constants/placeholders.ts
@@ -1,18 +1,19 @@
 type PhoneNumbersPlaceholders = {
-    [key: string]: string;
-  };
+  [key: string]: string;
+};
 
-  export const phoneNumbersPlaceholders: PhoneNumbersPlaceholders = {
-    EC: '0987654321',
-    CO: '3167654321',
-    MX: '74787654321',
-    AR: '91187654321',
-    US: '40887654321',
-    DO: '82987654321',
-    CA: '43787654321',
-    HN: '27654321',
-    GT: '47454321',
-    ES: '674654321',
-    FR: '661654321',
-    HK: '800654321'
-  }
+export const phoneNumbersPlaceholders: PhoneNumbersPlaceholders = {
+  EC: "0987654321",
+  CO: "3167654321",
+  MX: "74787654321",
+  AR: "91187654321",
+  US: "40887654321",
+  DO: "82987654321",
+  CA: "43787654321",
+  HN: "27654321",
+  GT: "47454321",
+  ES: "674654321",
+  FR: "661654321",
+  HK: "800654321",
+  IT: "34787654321",
+};
