a4742d5c11cac8d36a7f70da9f2d508f0608b33e
Jesus Sanchez <jhsanro@gmail.com>
2025-04-24 17:17:52 -0500

    Fix build

--- app/components/feedback/questions/BooleanQuestion.tsx
+++ app/components/feedback/questions/BooleanQuestion.tsx
@@ -1,4 +1,10 @@
-import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/app/components/ui/Form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/app/components/ui/Form";
 import { Path, UseFormReturn } from "react-hook-form";
 import { HootersFeedbackProps } from "@/app/validators/hootersFeedbackSchema";
 import React, { useState } from "react";
@@ -7,17 +13,18 @@ import Stack from "@mui/material/Stack";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { GusFeedbackProps } from "@/app/validators/gusFeedbackSchema";
+import useGetBusinessData from "@/app/hooks/useGetBusinessData";
 
 interface BooleanQuestionProps<T extends GusFeedbackProps> {
-  form: UseFormReturn<T>
-  question: string
-  yesButton: string
-  noButton: string
-  handleResponse: (answer: boolean) => void
-  prevStep: () => void
-  nextStep?: () => void
-  isQuestionClicked: React.MutableRefObject<boolean | null>
-  formName: Path<T>
+  form: UseFormReturn<T>;
+  question: string;
+  yesButton: string;
+  noButton: string;
+  handleResponse: (answer: boolean) => void;
+  prevStep: () => void;
+  nextStep?: () => void;
+  isQuestionClicked: React.MutableRefObject<boolean | null>;
+  formName: Path<T>;
 }
 
 export default function BooleanQuestion<T extends GusFeedbackProps>({
@@ -29,8 +36,9 @@ export default function BooleanQuestion<T extends GusFeedbackProps>({
   prevStep,
   nextStep,
   isQuestionClicked,
-  formName
+  formName,
 }: BooleanQuestionProps<T>) {
+  const { brandColor } = useGetBusinessData();
   const [isYesSelected, setIsYesSelected] = useState(false);
   const [isNoSelected, setIsNoSelected] = useState(false);
 
@@ -40,52 +48,66 @@ export default function BooleanQuestion<T extends GusFeedbackProps>({
         control={form.control}
         name={formName}
         render={({ field }) => (
-          <FormItem className='md:grid md:space-y-0 md:items-center md:gap-12'>
+          <FormItem className="md:grid md:space-y-0 md:items-center md:gap-12">
             <Stack spacing={2}>
-              <FormLabel className='col-span-3 text-question text-lg'>
+              <FormLabel className="col-span-3 text-question text-lg">
                 {question}
               </FormLabel>
-              <div className='flex items-start justify-center'>
-                <span className='self-start'>
-                  <a style={{ cursor: "pointer", marginTop: "30px" }} onClick={prevStep}>
-                    <ChevronLeftIcon className='text-gus' style={{ fontSize: 54, fontWeight: 'bolder' }} />
+              <div className="flex items-start justify-center">
+                <span className="self-start">
+                  <a
+                    style={{ cursor: "pointer", marginTop: "30px" }}
+                    onClick={prevStep}
+                  >
+                    <ChevronLeftIcon
+                      className="text-gus"
+                      style={{ fontSize: 54, fontWeight: "bolder" }}
+                    />
                   </a>
                 </span>
                 <FormControl>
                   <div className="flex justify-center space-x-4">
                     <Button
-                      variant={isQuestionClicked.current !== null && isQuestionClicked.current ? 'gusPrimary' : 'gusSecondary'}
-                      size={'hootersPrimary'} type={'button'} onClick={() => {
+                      color={`hsl(${brandColor || "var(--qik)"})`}
+                      type={"button"}
+                      onClick={() => {
                         setIsYesSelected(!isYesSelected);
                         setIsNoSelected(false);
                         isQuestionClicked.current = true;
                         field.onChange(true);
                         handleResponse(true);
-                      }}>
+                      }}
+                    >
                       {yesButton}
                     </Button>
                     <Button
-                      variant={isQuestionClicked.current !== null && !isQuestionClicked.current ? 'gusPrimary' : 'gusSecondary'}
-                      size={'hootersPrimary'} type={'button'} onClick={() => {
+                      color={`hsl(${brandColor || "var(--qik)"})`}
+                      type={"button"}
+                      onClick={() => {
                         setIsNoSelected(!isNoSelected);
                         setIsYesSelected(false);
                         isQuestionClicked.current = false;
                         field.onChange(false);
                         handleResponse(false);
-                      }}>
+                      }}
+                    >
                       {noButton}
                     </Button>
                   </div>
                 </FormControl>
-                {
-                  nextStep && (
-                    <span>
-                      <a style={{ cursor: "pointer", marginTop: "30px" }} onClick={nextStep}>
-                        <ChevronRightIcon className="text-gus" style={{ fontSize: 54, fontWeight: "bolder" }} />
-                      </a>
-                    </span>
-                  )
-                }
+                {nextStep && (
+                  <span>
+                    <a
+                      style={{ cursor: "pointer", marginTop: "30px" }}
+                      onClick={nextStep}
+                    >
+                      <ChevronRightIcon
+                        className="text-gus"
+                        style={{ fontSize: 54, fontWeight: "bolder" }}
+                      />
+                    </a>
+                  </span>
+                )}
               </div>
               <FormMessage />
             </Stack>
@@ -93,5 +115,5 @@ export default function BooleanQuestion<T extends GusFeedbackProps>({
         )}
       />
     </>
-  )
+  );
 }
